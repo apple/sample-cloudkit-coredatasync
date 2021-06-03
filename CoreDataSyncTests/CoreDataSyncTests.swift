db59c1d5242a8d6efaa3daf1794bf5784b5c8e12
Simon Manning <simon.manning@apple.com>
2021-06-03 14:20:07 -0400

    Conversion to Swift Concurrency API

--- CoreDataSyncTests/CoreDataSyncTests.swift
+++ CoreDataSyncTests/CoreDataSyncTests.swift
@@ -9,29 +9,25 @@ import CloudKit
 
 class CoreDataSyncTests: XCTestCase {
 
-    func test_CloudKitReadiness() throws {
+    func test_CloudKitReadiness() async throws {
         // Fetch zones from the Private Database of the CKContainer for the current user to test for valid/ready state
         let container = CKContainer(identifier: Config.containerIdentifier)
         let database = container.privateCloudDatabase
 
-        let fetchExpectation = expectation(description: "Expect CloudKit fetch to complete")
-        database.fetchAllRecordZones { _, error in
-            if let error = error as? CKError {
-                switch error.code {
-                case .badContainer, .badDatabase:
-                    XCTFail("Create or select a CloudKit container in this app target's Signing & Capabilities in Xcode")
+        do {
+            _ = try await database.allRecordZones()
+        } catch let error as CKError {
+            switch error.code {
+            case .badContainer, .badDatabase:
+                XCTFail("Create or select a CloudKit container in this app target's Signing & Capabilities in Xcode")
 
-                case .permissionFailure, .notAuthenticated:
-                    XCTFail("Simulator or device running this app needs a signed-in iCloud account")
+            case .permissionFailure, .notAuthenticated:
+                XCTFail("Simulator or device running this app needs a signed-in iCloud account")
 
-                default:
-                    XCTFail("CKError: \(error)")
-                }
+            default:
+                XCTFail("CKError: \(error)")
             }
-            fetchExpectation.fulfill()
         }
-
-        waitForExpectations(timeout: 10)
     }
 
 }
